a1ac924e31ecad49b429208ecaef1d76f472e604
Mbokeno <132112593+Mbokeno@users.noreply.github.com>
2024-09-05 17:04:33 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure this matches your deployment settings
+  },
+  base: '/', // Adjust this if your project is not at the root
+}
 })
